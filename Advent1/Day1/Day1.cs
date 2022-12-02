a79d5e8410230d4a4121f27ac8e0e5224cfa1f88
Andreas Andersson <me@andersson-andreas.se>
2022-12-02 19:42:25 +0100

    Restructuring of solution ad added day 2

---
+++ Advent1/Day1/Day1.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Day1
+{
+    internal class Day1
+    {
+        internal static void Run(string[] args)
+        {
+
+            Console.WriteLine("Starting elf calorie counting and stopwatch!");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            // Key is elf number
+            // Value is linked list of all cal numbers for that elf
+            var calVals = new Dictionary<int, LinkedList<int>>
+            {
+                { 1, new LinkedList<int>() }
+            };
+
+            // Assume we should think "normally", not as a developer. Hence 1 is first, not 0.
+            int elfCounter = 1;
+
+            // Read all lines and add all calorie-numbers for each elf
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    elfCounter++;
+                    calVals.Add(elfCounter, new LinkedList<int>());
+                    continue;
+                }
+
+                calVals[elfCounter].AddLast(int.Parse(line));
+            }
+
+            // Sum all the calories for each elf
+            var totalCalsByElf = calVals
+                .ToDictionary(
+                    x => x.Key,
+                    x => x.Value.Sum()
+                    )
+                .OrderByDescending(x => x.Value)
+                .ToList();
+
+            // For funz and verification...
+            //foreach(var totalCal in totalCalsByElf)
+            //{
+            //    Console.WriteLine($"Elf nr '{totalCal.Key}' has '{totalCal.Value}' calories");
+            //}
+
+            // Find the elf with most calories
+            var elfWithMostCaloriesWithTotal = totalCalsByElf.First();
+
+            Console.WriteLine($"Elf nr '{elfWithMostCaloriesWithTotal.Key}' has most calories, which is '{elfWithMostCaloriesWithTotal.Value}'");
+
+
+            // Part 3
+            var threeTopElves = totalCalsByElf.Take(3).ToList();
+
+            var totalCalsForTopThree = threeTopElves.Select(x => x.Value).Sum();
+
+            Console.WriteLine($"Top three elves is {string.Join(",", threeTopElves.Select(x => x.Key))} and have '{totalCalsForTopThree}' calories in total");
+
+            sw.Stop();
+
+            Console.WriteLine($"Execution took {sw.Elapsed.TotalMilliseconds:00.0}ms");
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Advent1/Day2/Day2.cs
@@ -0,0 +1,183 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Day2
+{
+    public class Day2
+    {
+        private static readonly Dictionary<GameOption, int> ScoresByOption = new Dictionary<GameOption, int>
+        {
+            { GameOption.Rock, 1 },
+            { GameOption.Paper, 2 },
+            { GameOption.Scissor, 3 },
+        };
+
+        // Dictionary<TheirChoice, Dictionary<MyChoice, MyScore>>
+        private static readonly Dictionary<GameOption, Dictionary<GameOption, int>> ScoreMatrix =
+            new Dictionary<GameOption, Dictionary<GameOption, int>>
+            {
+                { 
+                    GameOption.Rock, new Dictionary<GameOption, int>
+                    {
+                        { GameOption.Rock, 3+ScoresByOption[GameOption.Rock] },
+                        { GameOption.Paper, 6+ScoresByOption[GameOption.Paper] },
+                        { GameOption.Scissor, 0+ScoresByOption[GameOption.Scissor] }
+                    }
+                },
+                {
+                    GameOption.Paper, new Dictionary<GameOption, int>
+                    {
+                        { GameOption.Rock, 0+ScoresByOption[GameOption.Rock] },
+                        { GameOption.Paper, 3+ScoresByOption[GameOption.Paper] },
+                        { GameOption.Scissor, 6+ScoresByOption[GameOption.Scissor] }
+                    }
+                },
+                {
+                    GameOption.Scissor, new Dictionary<GameOption, int>
+                    {
+                        { GameOption.Rock, 6+ScoresByOption[GameOption.Rock] },
+                        { GameOption.Paper, 0+ScoresByOption[GameOption.Paper] },
+                        { GameOption.Scissor, 3+ScoresByOption[GameOption.Scissor] }
+                    }
+                }
+            };
+
+        // Dictionary<TheirChoice, Dictionary<DesiredOutcome, MyChoice>>
+        private static readonly Dictionary<GameOption, Dictionary<GameResult, GameOption>> ChoiceMatrix =
+            new Dictionary<GameOption, Dictionary<GameResult, GameOption>>
+            {
+                {
+                    GameOption.Rock, new Dictionary<GameResult, GameOption>
+                    {
+                        { GameResult.Lose, GameOption.Scissor },
+                        { GameResult.Draw, GameOption.Rock },
+                        { GameResult.Win, GameOption.Paper }
+                    }
+                },
+                {
+                    GameOption.Paper, new Dictionary<GameResult, GameOption>
+                    {
+                        { GameResult.Lose, GameOption.Rock },
+                        { GameResult.Draw, GameOption.Paper },
+                        { GameResult.Win, GameOption.Scissor }
+                    }
+                },
+                {
+                    GameOption.Scissor, new Dictionary<GameResult, GameOption>
+                    {
+                        { GameResult.Lose, GameOption.Paper },
+                        { GameResult.Draw, GameOption.Scissor },
+                        { GameResult.Win, GameOption.Rock }
+                    }
+                }
+            };
+
+        private enum GameOption
+        {
+            Rock = 0, // Opponent = A, You = X
+            Paper = 1, // Opponent = B, You = Y
+            Scissor = 2, // Opponent = C, You = Z
+        }
+
+        private enum GameResult
+        {
+            Lose = 0, // = X
+            Draw = 1, // = Y
+            Win = 2, // = Z
+        }
+
+        public static void Run(string[] args)
+        {
+
+            Console.WriteLine("Starting elf calorie counting and stopwatch!");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            int rowCount = 1;
+            var rowGameChoices = new Dictionary<int, (GameOption opponentsChoice, GameOption yourChoice)>();
+            var rowGameResults = new Dictionary<int, (GameOption opponentsChoice, GameResult desiredResult)>();
+            using (var fileReader = File.OpenText("Day2/input2.txt"))
+            {
+                do
+                {
+                    var currentLine = fileReader.ReadLine()?.Split(' ');
+
+                    if(currentLine == null || currentLine.Length != 2) continue;
+
+                    rowGameChoices.Add(rowCount, (CharToOption(currentLine[0][0]), CharToOption(currentLine[1][0])));
+                    rowGameResults.Add(rowCount, (CharToOption(currentLine[0][0]), CharToResult(currentLine[1][0])));
+
+                    rowCount++;
+
+                } while (!fileReader.EndOfStream);
+            }
+
+            var allScores = rowGameChoices
+                .ToDictionary(
+                    x => x.Key, 
+                    x => ScoreMatrix[x.Value.opponentsChoice][x.Value.yourChoice]
+                    );
+
+            var totalScore = allScores.Values.Sum();
+
+            var allResults = rowGameResults
+                .ToDictionary(
+                    x => x.Key, 
+                    x => ChoiceMatrix[x.Value.opponentsChoice][x.Value.desiredResult]
+                    );
+
+            var allScoresActual = rowGameChoices
+                .ToDictionary(
+                    x => x.Key,
+                    x => ScoreMatrix[x.Value.opponentsChoice][allResults[x.Key]]
+                );
+
+            var totalScoreActual = allScoresActual.Values.Sum();
+
+            sw.Stop();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total matches: " + allScores.Count);
+
+            Console.WriteLine("Total score: " + totalScore);
+
+            Console.WriteLine("Total score actual: " + totalScoreActual);
+
+            Console.WriteLine($"Execution took {sw.Elapsed.TotalMilliseconds:0.}ms");
+
+            Console.ReadKey();
+        }
+        
+        private static GameOption CharToOption(char currentChar)
+        {
+            switch (currentChar)
+            {
+                case 'A' or 'X':
+                    return GameOption.Rock;
+                case 'B' or 'Y':
+                    return GameOption.Paper;
+                case 'C' or 'Z':
+                    return GameOption.Scissor;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static GameResult CharToResult(char currentChar)
+        {
+            switch (currentChar)
+            {
+                case'X':
+                    return GameResult.Lose;
+                case 'Y':
+                    return GameResult.Draw;
+                case 'Z':
+                    return GameResult.Win;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+
+}
